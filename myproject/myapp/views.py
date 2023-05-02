df7031a49eefdd0647af53be4883a41e3c0e1c46
Aman <amanpreet@techindustan.com>
2023-05-02 12:02:43 +0530

    first commit

---
+++ myproject/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def aman(request):
+    return HttpResponse("<h1>My name is aman</h1>")
+
+
+def home(request):
+    return render(request , "myapp/aman.html" )
